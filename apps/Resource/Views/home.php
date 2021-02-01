d8683e853e296ecfd2bb9012b8b3d1c6aefef048
eggy fachrurrozi <eggifahrurrozy21@gmail.com>
2021-02-01 20:55:06 +0700

    Update home.php

--- apps/Resource/Views/home.php
+++ apps/Resource/Views/home.php
@@ -4,33 +4,19 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Ex</title>
-
-        
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        
+        <title>Ex</title>    
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">   
         <link rel="stylesheet" href="/css/main.css">
-         <link rel="stylesheet" href="/Ex-code-1.0.0/public/css/main.css">
-        
-        
-       
-        
+        <link rel="stylesheet" href="/Ex-code-1.0.0/public/css/main.css">     
     </head>
     <body>
-        
-           
-
             <div class="content">
                 <div class="title m-b-md">
                   Ex - Codes  >>
                 </div>
-                </div>
-
                 <div class="mini">
-                <a> Play awasome codes!!! </a>
-                
+                  <a> Play awasome codes!!! </a>
                 </div>
-          
+            </div>
     </body>
 </html>
